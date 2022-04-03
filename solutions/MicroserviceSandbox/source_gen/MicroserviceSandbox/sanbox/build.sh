908b810c638dff17e6ccc54dac4df0e996cade04
Claudiu Ciumedean <claudiuciumedean@gmail.com>
2022-04-03 20:17:58 +0300

    deleted generated file

--- solutions/MicroserviceSandbox/source_gen/MicroserviceSandbox/sanbox/build.sh
+++
@@ -1,9 +0,0 @@
-# sed -i -e 's/\r$//' build.sh 
-# chmod 755 build.sh
-# ./build.sh
-
-# Build docker image
-docker build -t customerservice .
-
-# Build docker container
-docker run -d -p 3000:5000 --name customerservicepython customerservice
